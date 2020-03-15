e0a11a7030fc7462884961b357e671d4c51df955
EvaBat <evabat13@gmail.com>
2020-03-15 22:31:32 +0200

    attempt to fix again cross origin

--- routes/paths.js
+++ routes/paths.js
@@ -9,14 +9,13 @@ const jwt = require("jsonwebtoken");
 const Path = require("../models/Path");
 
 module.exports = (app) => {
-  const allowCrossDomain = function(req, res, next) {
+  app.all('/', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-  };
-
-  app.use(allowCrossDomain);
+  });
+  
 // Bodyparser middleware
   app.get(`/api/paths`, async (req, res) => {
     try {
