9d78c6d2d1500422f6acfaf8e4d4473615292ced
EvaBat <evabat13@gmail.com>
2020-03-15 22:50:44 +0200

    minor fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const paths = require("./routes/paths");
 const app = express();
 
 const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://devabat.github.io/');
+    res.header('Access-Control-Allow-Origin', 'https://devabat.github.io');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
